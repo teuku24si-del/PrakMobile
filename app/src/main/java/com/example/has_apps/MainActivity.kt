79bee61e4c446c9429da4339d89a3b7eadceb382
lab151-27 <teuku24si@mahasiswa.pcr.ac.id>
2026-04-06 10:20:34 +0700

    Pertemuan 4

--- app/src/main/java/com/example/has_apps/MainActivity.kt
+++ app/src/main/java/com/example/has_apps/MainActivity.kt
@@ -1,20 +1,42 @@
 package com.example.has_apps
 
+import com.example.has_apps.Pertemuan_4.FourthActivity
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.has_apps.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        binding= ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+//        val btnKirm : Button = findViewById(R.id.btnKirim)
+//        val noTujuan : EditText = findViewById(R.id.inputNoTujuan)
+
+        binding.btnToFourth.setOnClickListener {
+            val btnToFourth = binding.btnToFourth.text
+            Toast.makeText(this,"Pesan berhasil dikirim ke $btnToFourth" , Toast.LENGTH_SHORT).show()
+            val intent = Intent(this, FourthActivity::class.java)
+
+            /*tambahkan bagian berikut*/
+            intent.putExtra("name", "nama")
+            intent.putExtra("from", "asal")
+            intent.putExtra("age", 25)
+
+
+            startActivity(intent)
         }
     }
 }
---
+++ app/src/main/java/com/example/has_apps/Pertemuan_4/FourthActivity.kt
@@ -0,0 +1,76 @@
+package com.example.has_apps.Pertemuan_4
+
+import android.os.Bundle
+import android.util.Log
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.has_apps.R
+import com.example.has_apps.databinding.ActivityFourthBinding
+import com.example.has_apps.databinding.ActivityMainBinding
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
+import com.google.android.material.snackbar.Snackbar
+
+class FourthActivity : AppCompatActivity() {
+    lateinit var binding: ActivityFourthBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        Log.e("onCreate", "{Fourthactivity} dibuat pertama kali")
+        enableEdgeToEdge()
+        binding= ActivityFourthBinding.inflate(layoutInflater)
+
+        setContentView(binding.root)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+
+        val name = intent.getStringExtra("name")
+        val from = intent.getStringExtra("from")
+        val age = intent.getIntExtra("age",0)
+        Log.e("Data Intent","Nama: $name , Usia: $age, Asal: $from")
+
+        binding.btnShowSnackbar.setOnClickListener {
+            Snackbar.make(binding.root, "Ini adalah Snackbar", Snackbar.LENGTH_SHORT)
+                .setAction("Tutup"){
+                    Log.e("Info Snackbar","Snackbar ditutup")
+                }
+                .show()
+        }
+
+        binding.btnShowAlertDialog.setOnClickListener {
+            MaterialAlertDialogBuilder(this)
+                .setTitle("Konfirmasi")
+                .setMessage("Apakah Anda yakin ingin melanjutkan?")
+                .setPositiveButton("Ya") { dialog, _ ->
+                    dialog.dismiss()
+                    Log.e("Info Dialog","Anda memilih Ya!")
+                }
+                .setNegativeButton("Batal") { dialog, _ ->
+                    dialog.dismiss()
+                    Log.e("Info Dialog","Anda memilih Tidak!")
+                }
+                .show()
+        }
+
+        binding.btnKembali.setOnClickListener {
+            val btnKembali = binding.btnKembali.text
+            finish()
+        }
+
+
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.e("onStart", "onStart: {Mainactivity} terlihat di layar")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.e("onDestroy", "{Fourthactivity} dihapus dari stack")
+    }
+}
